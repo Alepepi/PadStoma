15a22d821704cf4416304a81ac5ddc5d2013abcd
alepepi <aleollivierochoa@gmail.com>
2025-05-07 17:34:28 +0200

    Add(Menu): Rest of the menu and iteration of images

--- PadStoma/ContentView.swift
+++ PadStoma/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let images = ["StomaBag", "StomaBag2", "StomaBag3"]
+    let rotations: [Double] = [-45, -35, 0, 35, 45]
+
     var body: some View {
         VStack {
+            
             HStack{
                 Button("", systemImage: "gearshape.fill"){
                     print("Play")
@@ -21,36 +25,83 @@ struct ContentView: View {
                 Spacer()
             }
             
-            Image("logo")
-                  .resizable()
-                  .aspectRatio(contentMode: .fit)
-                  .frame(maxWidth: 500, maxHeight: 300)
-                  .padding()
-                  //.border(Color.black)
+                Image("VectorStomaDida")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 500)
+                    .padding(.bottom, 100)
+                
+                
+                Button{
+                    print("Play")
+                } label: {
+                    HStack {
+                        Image(systemName: "play.fill")
+                        Spacer()
+                        Text("Play")
+                        Spacer()
+                    }
+                }
+                .padding()
             
-            Spacer()
+                .font(.system(size: 50, weight: .regular, design: .default))
+                .foregroundColor(.white)
+                .frame(width: 350)
+                .background(Color.bluePrimary)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 20)
+                ).overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.clear)
+                        .stroke(Color.blueSecond, lineWidth: 3)
+                }
+                .padding()
 
-            
-            Button(" Play ", systemImage: "play.fill"){
-                print("Play")
+            Button{
+                print("Tools")
+            } label: {
+                HStack {
+                    Image(systemName: "cross.case")
+                    Spacer()
+                    Text("Tools")
+                    Spacer()
+                }
             }
             .padding()
+        
             .font(.system(size: 50, weight: .regular, design: .default))
             .foregroundColor(.white)
+            .frame(width: 350)
             .background(Color.bluePrimary)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .padding()
-            
-            Button(" Tools ", systemImage: "cross.case"){
-                print("Play")
+            .clipShape(
+                RoundedRectangle(cornerRadius: 20)
+            ).overlay {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.clear)
+                    .stroke(Color.blueSecond, lineWidth: 3)
             }
             .padding()
-            .font(.system(size: 50, weight: .regular, design: .default))
-            .foregroundColor(.white)
-            .background(Color.bluePrimary)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
             
             Spacer()
+            
+            ScrollView (.horizontal) {
+                HStack {
+                    ForEach(images, id: \.self) { imageName in
+                        ForEach(rotations, id: \.self) { angle in
+                            Image(imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .rotationEffect(.degrees(angle))
+                                .frame(maxWidth: 300, maxHeight: 150)
+                                .padding()
+
+                            Spacer(minLength: 20)
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+            
         }
         .padding()
         .background(Color(red: 208, green: 247, blue: 245))
