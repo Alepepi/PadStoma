a80a07ca68b23ecc6586875300b18276295f0595
alepepi <aleollivierochoa@gmail.com>
2025-05-08 15:57:15 +0200

    Add(ToolsView): New view of the tools screen

---
+++ PadStoma/ToolsView.swift
@@ -0,0 +1,18 @@
+//
+//  ToolsView.swift
+//  PadStoma
+//
+//  Created by Alejandro Ollivier Ochoa on 5/8/25.
+//
+
+import SwiftUI
+
+struct ToolsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolsView()
+}
