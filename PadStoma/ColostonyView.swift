52ed2d6b5e52ac5de740ef15ec33c11825bc70d4
Frida Perez Perfecto <frida_perez@hotmail.com>
2025-05-08 20:24:27 +0200

    Implementation of router, optimisation of old views and new views

---
+++ PadStoma/ColostonyView.swift
@@ -0,0 +1,7 @@
+//
+//  Colostony.swift
+//  PadStoma
+//
+//  Created by Frida Pérez Perfecto on 08/05/25.
+//
+
---
+++ PadStoma/PlayView.swift
@@ -0,0 +1,16 @@
+//
+//  SimulatorView.swift
+//  PadStoma
+//
+//  Created by Frida Pérez Perfecto on 08/05/25.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Text("PlayView!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
