82e813ba973bfcdeac964e7e1ebbb115f80d6f60
Alejandro Ollivier <aleollivierochoa@gmail.com>
2025-05-11 21:15:20 +0200

    chore: fix linting

--- PadStoma/Views/Components/AnnaChatBubble.swift
+++ PadStoma/Views/Components/AnnaChatBubble.swift
@@ -14,11 +14,7 @@ struct ChatBotOverlay: View {
                         .font(.body)
                         .transition(.slide)
                     Button(action: {
-                        if currentStep < steps.count - 1 {
-                            currentStep+=1
-                        }
-                    })
-                    {
+                        if currentStep < steps.count - 1 {currentStep+=1}}) {
                         Text("Next")
                             .font(.system(size: 17, weight: .bold))
                             .foregroundColor(.blue)
