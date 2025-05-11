a156c0186d07733c1d72211c98cd36b28391b617
Alejandro Ollivier <aleollivierochoa@gmail.com>
2025-05-11 18:08:40 +0200

    chore: fix linting

--- PadStoma/Views/ColostomyView.swift
+++ PadStoma/Views/ColostomyView.swift
@@ -43,7 +43,7 @@ struct ColostomyView: View {
                     }
                 }
                 .padding(.horizontal)
-             }
+            }
             .padding()
             .background(Color.bluePrimary)
             .cornerRadius(20)
